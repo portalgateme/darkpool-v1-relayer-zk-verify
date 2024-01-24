12448685bc8fa8f944d73b620cd9648d9f9e5b9e
Peter0x2 <peter0x2@outlook.com>
2024-01-24 12:05:18 +0800

    change contract address according to dev deployment

--- src/config/pgDarkPoolConfig.js
+++ src/config/pgDarkPoolConfig.js
@@ -14,7 +14,7 @@ module.exports = {
     //deployedBlock: 9739503,
   },
   netId31337: {
-    pgDarkPoolAssetManager: '0x9A676e781A523b5d0C0e43731313A708CB607508',
+    pgDarkPoolAssetManager: '0x0B306BF915C4d645ff596e518fAf3F9669b97016',
     pgDarkPoolVerifierHub: '0x610178dA211FEF7D417bC0e6FeD39F05609AD788',
     pgDarkPoolUniswapAssetManager:'',
     pgDarkPoolCurveAssetManager:'',
