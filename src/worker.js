f9c98fc667661db4fdf41baa95178302d6a5468f
Peter0x2 <peter0x2@outlook.com>
2024-02-05 13:34:06 +0800

    align contract interface of withdraw

--- src/worker.js
+++ src/worker.js
@@ -136,9 +136,9 @@ async function getTxObject({ data }) {
         throw new RelayerError('Invalid proof')
     }
     if(isETH(data.asset)){
-      calldata = darkPoolContract.methods.withdrawETH(data.proof, data.merkleRoot, data.nullifier, data.recipient, data.relayer, data.amount).encodeABI()
+      calldata = darkPoolContract.methods.withdrawETH(data.proof, data.merkleRoot, data.nullifier, data.recipient, data.relayer,data.refund, data.amount).encodeABI()
     }else{
-      calldata = darkPoolContract.methods.withdrawERC20(data.asset, data.assetMod, data.proof, data.merkleRoot, data.nullifier, data.recipient, data.relayer, data.amount).encodeABI()
+      calldata = darkPoolContract.methods.withdrawERC20(data.asset, data.assetMod, data.proof, data.merkleRoot, data.nullifier, data.recipient, data.relayer,data.refund, data.amount).encodeABI()
     }  
     return {
       to: darkPoolContract._address,
