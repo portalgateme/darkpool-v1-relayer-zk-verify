f7688f23cbc0873c2d07262419499996608ccbd1
Peter0x2 <peter0x2@outlook.com>
2024-02-19 14:08:15 +0800

    update latest address

--- src/config/pgDarkPoolConfig.js
+++ src/config/pgDarkPoolConfig.js
@@ -20,13 +20,13 @@ module.exports = {
     //deployedBlock: 9739503,
   },
   netId31337: {
-    verifierHub: '0x4f1F87d512650f32bf9949C4c5Ef37a3cc891C6D',
-    merkleTreeOperator: '0xad203b3144f8c09a20532957174fc0366291643c',
-    darkpoolAssetManager: '0xabebE9a2D62Af9a89E86EB208b51321e748640C3',
-    uniswapAssetManager: '0x82A9286dB983093Ff234cefCea1d8fA66382876B',
-    curveMultiExchangeAssetManager: '0x59c7D03d2E9893FB7bAa89dA50a9452e1e9B8b90',
-    curveAddLiquidityAssetManager: '0x29023DE63D7075B4cC2CE30B55f050f9c67548d4',
-    curveRemoveLiquidityAssetManager: '0xCC5Bc84C3FDbcF262AaDD9F76652D6784293dD9e',
+    verifierHub: '0x29023DE63D7075B4cC2CE30B55f050f9c67548d4',
+    merkleTreeOperator: '0x876939152C56362e17D508B9DEA77a3fDF9e4083',
+    darkpoolAssetManager: '0x82A9286dB983093Ff234cefCea1d8fA66382876B',
+    uniswapAssetManager: '0x6431AF84d34F0522cAA58b221d94A150B5AdAC69',
+    curveMultiExchangeAssetManager: '0xCC5Bc84C3FDbcF262AaDD9F76652D6784293dD9e',
+    curveAddLiquidityAssetManager: '0x59c7D03d2E9893FB7bAa89dA50a9452e1e9B8b90',
+    curveRemoveLiquidityAssetManager: '0x3Af511B1bdD6A0377e23796aD6B7391d8De68636',
     //deployedBlock: 9739503,
   }
 }
