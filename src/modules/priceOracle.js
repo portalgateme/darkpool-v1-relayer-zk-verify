3a62f22d178ac8a7c68c539ccc955a6675770657
Peter0x2 <peter0x2@outlook.com>
2024-04-16 21:56:40 +0800

    add fallback price oracle

---
+++ src/modules/priceOracle.js
@@ -0,0 +1,36 @@
+const { netId } = require('../config/config')
+const priceOracleConfigs = require('../config/priceOracleConfig')
+const axios = require('axios')
+const ethers = require('ethers')
+
+const priceOracleConfig = priceOracleConfigs[netId]
+
+async function getPriceToNativeFromLLama(assets) {
+    let tokens = []
+    const nativeToken = "coingecko:" + priceOracleConfig.defillamaNativeCoingecoId
+    tokens.push(nativeToken)
+    assets.forEach(asset => {
+        tokens.push(priceOracleConfig.defillamaChainPrefix + ":" + asset)
+    })
+
+    const response = await axios.get(`${priceOracleConfig.defillamaUrl}/prices/current/${tokens.join(',')}?searchWidth=4h`)
+    console.log(response)
+
+    const nativeDecimal = priceOracleConfig.defillamaNativeDecimals;
+    const nativePrice = ethers.utils.parseUnits(response.data['coins'][nativeToken].price.toString(), nativeDecimal);
+
+    const prices = {}
+    assets.forEach(asset => {
+        const priceData = response.data['coins'][priceOracleConfig.defillamaChainPrefix + ":" + asset]
+        const decimals = priceData.decimals;
+        const price = ethers.utils.parseUnits(priceData.price.toString(), decimals)
+        const priceToNative = ethers.BigNumber.from(price).mul(ethers.BigNumber.from(10).pow(nativeDecimal)).div(ethers.BigNumber.from(nativePrice)).toBigInt()
+        prices[asset] = priceToNative
+    })
+
+    return prices
+}
+
+module.exports = {
+    getPriceToNativeFromLLama
+}
