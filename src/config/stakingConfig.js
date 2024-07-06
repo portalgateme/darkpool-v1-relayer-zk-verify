f21eead1702161ae9f80d889b4cd3d7b5febc9e0
Peter0x2 <peter0x2@outlook.com>
2024-07-06 18:21:03 +0800

    update bb contract addrss

--- src/config/stakingConfig.js
+++ src/config/stakingConfig.js
@@ -45,6 +45,27 @@ const stakingTokenConfig = {
       name: 'sgUSDC',
     },
   ],
+  [ChainId.BounceBit]: [
+    {
+      originalToken: '0x0000000000000000000000000000000000000000',
+      stakingToken: '0x2E7Ed17CCDF306B7eB301A05b3CB895f5b4F1643',
+      name: 'sgBB',
+    },
+    {
+      originalToken: '0xF5e11df1ebCf78b6b6D26E04FF19cD786a1e81dC',
+      stakingToken: '0xF99969B1Cb7255e8F14741eAFEEdC767e743899a',
+      name: 'sgBBTC',
+    },
+    {
+      originalToken: '0x77776b40C3d75cb07ce54dEA4b2Fd1D07F865222',
+      stakingToken: '0x45AAD8B921BB0e3f88A108047D743Ca6388aFe49',
+      name: 'sgBBUSDC',
+    },
+    {
+      originalToken: '0x22aAC17E571D6651880d057e310703fF4C7c3483',
+      stakingToken: '0xfBf0dDceF9360757fCA368911b6719a35DD8C660',
+      name: 'sgSTBB',
+  ],
   [ChainId.POLYGON]: [],
   [ChainId.SEPOLIA]: [],
   [ChainId.BounceBitTestnet]: [
